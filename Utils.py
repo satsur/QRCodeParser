a8e06c351237d5c24f897a9e339d85a5749c77e8
Andrew Galbraith Ryer <DrewTheRat@gmail.com>
2024-03-18 08:55:28 -0400

    Fixed bug with finding files in directory

--- Utils.py
+++ Utils.py
@@ -1,13 +1,13 @@
 import os
 
 def find_files(filename, search_path):
-   result = None
+   result = []
 
 # Walking top-down from the root
    for root, directory, files in os.walk(search_path):
       if filename in files:
          result.append(os.path.join(root, filename))
-   if result is not None and len(result) > 0:
+   if len(result) > 0:
       return result[0]
    else:
       return None
